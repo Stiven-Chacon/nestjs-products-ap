32363a2c2eb02be325f675f5732a611043d5285f
Stiven-Chacon <stivenchacon031@gmail.com>
2025-08-06 16:14:45 -0500

    feat: configure MongoDB connection using environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
