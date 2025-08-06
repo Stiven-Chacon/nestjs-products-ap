058a4a9596f13ada57c0700bc911657ffdc90f28
Stiven-Chacon <stivenchacon031@gmail.com>
2025-08-06 16:27:07 -0500

    add ProductsModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI!)],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI!), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
